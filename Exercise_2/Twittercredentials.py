668f24b01ea92e51f90eaeb7f02c25636d6dd6ea
Jackanator <jakzhang@ucdavis.edu>
2016-04-26 16:18:06 -0700

    Update credentials

--- Exercise_2/Twittercredentials.py
+++ Exercise_2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "4zZAQnA4Shx8fRpb7HfBzUrtT";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "9mpce2f6X7LqIJBs9ef0WiRpdsq8hx0Y9RFPg6xic2jfgczYrg";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "714258830490664960-cu4R5gUGHBBmfeshuXA5uJpWn4EQOsb";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "qT0YLj0hyq6IzJgMFBVJ057WjTxLhaGlw4HGGLlBwcOcW";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
