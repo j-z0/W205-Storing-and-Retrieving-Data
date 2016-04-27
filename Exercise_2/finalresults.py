06e1eda6bab5b11f7e74e519094734d4630fb2bb
Jackanator <jakzhang@ucdavis.edu>
2016-04-26 20:16:02 -0700

    Create finalresults.py

---
+++ Exercise_2/finalresults.py
@@ -0,0 +1,16 @@
+import sys
+import psycopg2
+
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+
+cur = conn.cursor()
+
+if len(sys.argv)>1:
+    cur.execute("SELECT count FROM tweetwordcount WHERE word=%s",  sys.argv[1:])
+    print "Total number of occurences of", str(sys.argv[1]), ":", cur.fetchone()
+else:
+    cur.execute("SELECT word, count FROM tweetwordcount ORDER BY word")
+    for record in cur:
+        print record
+
+print 'Testing: ', sys.argv[1:]
