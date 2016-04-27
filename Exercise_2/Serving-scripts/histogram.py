4ce3eb63675b4e2588e8f7e5ea5111bba4952b3c
Jackanator <jakzhang@ucdavis.edu>
2016-04-26 20:31:19 -0700

    Create histogram.py
    Serving script 2

---
+++ Exercise_2/Serving-scripts/histogram.py
@@ -0,0 +1,13 @@
+import sys
+import psycopg2
+
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+
+cur = conn.cursor()
+
+x = sys.argv[1].split(',')[0]
+y = sys.argv[1].split(',')[1]
+
+cur.execute("SELECT word,count FROM tweetwordcount WHERE count BETWEEN %s  AND %s", (x,y))
+for record in cur:
+    print record
